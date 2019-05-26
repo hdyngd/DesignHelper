af522b17ff68bde44a1ec2ce9c555f073607efd5
柳田英彦 <hide@satouhidehiko-no-MacBook-Air-2.local>
2019-05-26 19:19:53 +0900

    first commit

---
+++ resources/assets/js/components/About.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        This page describe who we are.
+    </div>
+</template>
+
+<script>
+    // export default {
+    //     name: "About.vue"
+    // }
+</script>
+
+<style scoped>
+
+</style>
---
+++ routes/api.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+Route::group(['middleware' => 'api'], function() {
+    Route::get('articles',  function() {
+//        $articles = Article::all()->take(5);
+        $articles = [
+            ['id' => 1, 'title' => 'taro', 'content' => 'aaa'],
+            ['id' => 2, 'title' => 'jiro', 'content' => 'bbb'],
+            ['id' => 3, 'title' => 'sabro', 'content' => 'ccc'],
+            ['id' => 4, 'title' => 'shiro', 'content' => 'ddd'],
+            ['id' => 5, 'title' => 'goro', 'content' => 'eee'],
+        ];
+        return $articles;
+    });
+});
+
+//Route::group(['middleware' => 'api'], function() {
+//    Route::get('articles',  'ArticleController@index');
+//});
