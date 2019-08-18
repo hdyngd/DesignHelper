db1d21a6be86a4fbf6dcc89cd8b223d6d6655cb1
hidehiko yanagida <hyanagida.0721@gmail.com>
2019-08-18 20:52:30 +0900

    クレジット決済プロト

--- resources/assets/js/components/pages/Profile.vue
+++ resources/assets/js/components/pages/Profile.vue
@@ -8,6 +8,13 @@
                 <profile-edit-form
                     :admin="false"
                 ></profile-edit-form>
+                <form id="mainform">
+                    <input id="tkn" name="tkn" type="hidden" value="">
+                    <div id="CARD_INPUT_FORM" />
+
+                    <el-button type="primary" @click="doPurchase()">購入する</el-button>
+
+                </form>
             </el-card>
         </el-col>
     </el-row>
@@ -16,6 +23,27 @@
 <script>
     export default {
         name: "Profile",
+        methods: {
+            doPurchase() {
+                CPToken.CardInfo (
+                    {
+                        aid: process.env.MIX_ROBOTPAYMENT_AID
+                    },
+                    this.execPurchase
+                );
+            },
+            execPurchase(resultCode, errMsg) {
+                if (resultCode != "Success") {
+                    // 戻り値がSuccess以外の場合はエラーメッセージを表示
+                    window.alert(errMsg);
+                } else {
+                    // スクリプトからフォームをsubmit
+                    $("#mainform").submit();
+                }
+            }
+
+        }
+
     }
 </script>
 
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -20,9 +20,40 @@
             csrfToken: "{{ csrf_token() }}"
         };
     </script>
+
+    <script type="text/javascript" src="https://credit.j-payment.co.jp/gateway/js/jquery.js"></script>
+    <script type="text/javascript" src="https://credit.j-payment.co.jp/gateway/js/CPToken.js"></script>
 </head>
 <body>
+{{--    <form id="mainform">--}}
+{{--        <input id="tkn" name="tkn" type="hidden" value="">--}}
+{{--        <div id="CARD_INPUT_FORM" />--}}
+{{--        <input type="button" value="購入する" onclick="doPurchase()"/>--}}
+{{--    </form>--}}
     <div id="app"></div>
+
     <script src="{{ mix('js/app.js') }}"></script>
+
+{{--    <script>--}}
+{{--        function doPurchase() {--}}
+{{--        //CP非同期通信よりカード番号入力画面を表示する--}}
+{{--            CPToken.CardInfo (--}}
+{{--            {--}}
+{{--                aid: '113408'--}}
+{{--            },--}}
+{{--            execPurchase );--}}
+{{--        }--}}
+
+{{--        // コールバック関数--}}
+{{--        function execPurchase(resultCode, errMsg) {--}}
+{{--            if (resultCode != "Success") {--}}
+{{--            // 戻り値がSuccess以外の場合はエラーメッセージを表示--}}
+{{--                window.alert(errMsg);--}}
+{{--            } else {--}}
+{{--            // スクリプトからフォームをsubmit--}}
+{{--                $("#mainform").submit();--}}
+{{--            }--}}
+{{--        }--}}
+{{--    </script>--}}
 </body>
 </html>
