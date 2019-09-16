9c7ea57c79fb9f254c6042751ac8a8f5a254a2ff
hidehiko yanagida <hyanagida.0721@gmail.com>
2019-09-16 15:41:22 +0900

    #46 プロフィール画面で戻るボタンを実装

--- resources/assets/js/components/pages/Profile.vue
+++ resources/assets/js/components/pages/Profile.vue
@@ -3,6 +3,8 @@
         <el-col :span="16" :offset="4">
             <el-card>
                 <div slot="header" class="clearfix">
+<!--                    <i class="el-icon-close" style="float: right; padding: 3px 0"><el-button @click="handleBack()"></el-button></i>-->
+                    <el-button style="float: right; padding: 3px 0" type="info" icon="el-icon-close" @click="handleBack()"></el-button>
                     <h3>プロフィール</h3>
                 </div>
                 <profile-edit-form
@@ -40,6 +42,9 @@
                     // スクリプトからフォームをsubmit
                     $("#mainform").submit();
                 }
+            },
+            handleBack() {
+                router.push('/');
             }
 
         }
