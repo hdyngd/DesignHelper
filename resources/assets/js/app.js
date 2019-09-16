60f80ec448b3804d40bafea0893f203ed6e1a90d
hidehiko yanagida <hyanagida.0721@gmail.com>
2019-09-16 14:24:01 +0900

    #44 elementui日本語化

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -38,6 +38,10 @@ if (token) {
 
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+import lang from 'element-ui/lib/locale/lang/ja'
+import locale from 'element-ui/lib/locale'
+locale.use(lang)
+
 // import App from './App.vue';
 
 Vue.use(ElementUI);
