934da5f38142b60b96f7b4aa4c6d6a0c84c6372f
hidehiko yanagida <hyanagida.0721@gmail.com>
2019-10-25 23:05:49 +0900

    #68 トップページ実装

---
+++ resources/assets/js/containers/top.js
@@ -0,0 +1,33 @@
+import { connect } from './connect'
+
+import Home from '../components/pages/Home'
+import store from "../store";
+export default connect({
+    gettersToProps: {
+        categories: 'getCategories',
+        isHome: 'getIsHome',
+    },
+    actionsToProps: {
+        // deleteWorkRoom: 'deleteWorkRoom',
+        // createWorkRoom: 'createWorkRoom',
+    },
+    lifecycle: {
+        beforeRouteEnter (to, from, next) {
+            if(to.query.tkn) {
+                store.dispatch('restoreShoppingCart', to.query.tkn)
+                store.dispatch('toggleShoppingCart', true)
+                store.dispatch('flushSuccess', {title: 'Success', message: 'クレジットカード情報が入力されました。'})
+            }
+
+            store.dispatch('setIsHome', true)
+
+            store.dispatch('fetchCategories')
+                .then(() => {
+                    next()
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        }
+    }
+})('home-component', Home)
